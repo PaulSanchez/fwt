599f9191b990662b0344f5c28815d7d85eefa2f2
Paul J Sanchez <pjs@alum.mit.edu>
2024-04-26 12:44:39 -0700

    no longer in-place

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,14 @@
 use std::ops::Add;
 use std::ops::Sub;
 
-// Perform a fast Walsh transformation using a Manz sequency ordered
-// in-place algorithm. The vector is modified by this algorithm.
-pub fn sequency<T>(v : &mut [T]) -> &mut [T]
+// Perform a fast Walsh transformation using Manz sequency ordering.
+pub fn sequency<T>(input_v : &[T]) -> Vec<T>
 where
      T : Add<Output = T> + Sub<Output = T> + Copy + std::ops::AddAssign<T>
 {
-    // let v: &[f64] = v.as_ref();
-    let length = v.len();
+    let length = input_v.len();
+    let mut v = input_v.to_vec();
+
     if !power_of_2(length.try_into().unwrap()) {
         panic!("sequency: vector length must be a power of 2")
     }
@@ -51,14 +51,13 @@ where
     v
 }
 
-// Perform a fast Walsh transformation using a Hadamard (natural) ordered
-// in-place algorithm. The vector is modified by this algorithm.
-pub fn hadamard<T>(v : &mut [T]) -> &mut [T]
+// Perform a fast Walsh transformation using Hadamard (natural) ordering.
+pub fn hadamard<T>(input_v : &[T]) -> Vec<T>
 where
      T : Add<Output = T> + Sub<Output = T> + Copy + std::ops::AddAssign<T>
 {
-    // let v: &[f64] = v.as_ref();
-    let length = v.len();
+    let length = input_v.len();
+    let mut v = input_v.to_vec();
     if !power_of_2(length.try_into().unwrap()) {
         panic!("hadamard: vector length must be a power of 2")
     }
@@ -80,7 +79,7 @@ where
 
 // Boolean test of whether an Integer is a pure power of two.
 // This is an O(1) algorithm.
-pub fn power_of_2(n: u32) -> bool {
+pub fn power_of_2(n: usize) -> bool {
     match n {
         0 => false,
         _ => n & (n - 1) == 0,
@@ -88,7 +87,7 @@ pub fn power_of_2(n: u32) -> bool {
 }
 
 // Scale a vector by its length
-pub fn scale<T>(v: &mut [T]) -> Vec<f64>
+pub fn scale<T>(v: &[T]) -> Vec<f64>
 where
     T: Copy,
     f64: From<T>
@@ -113,95 +112,98 @@ mod tests {
             [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
         );
         assert_eq!(
-            hadamard(&mut [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+            hadamard(&[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
             [1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0]
         );
         assert_eq!(
-            hadamard(&mut [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+            hadamard(&[0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
             [1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0]
         );
         assert_eq!(
-            hadamard(&mut [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0]),
+            hadamard(&[0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0]),
             [1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0]
         );
         assert_eq!(
-            hadamard(&mut [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]),
+            hadamard(&[0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]),
             [1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0]
         );
         assert_eq!(
-            hadamard(&mut [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0]),
+            hadamard(&[0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0]),
             [1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0]
         );
         assert_eq!(
-            hadamard(&mut [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]),
+            hadamard(&[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]),
             [1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0]
         );
         assert_eq!(
-            hadamard(&mut [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]),
+            hadamard(&[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]),
             [1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0]
         );
         assert_eq!(
-            hadamard(&mut [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]),
+            hadamard(&[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]),
             [1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0]
         );
         assert_eq!(
-            hadamard(&mut [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
+            hadamard(&[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]),
             [1, -1, -1, 1, -1, 1, 1, -1, -1, 1, 1, -1, 1, -1, -1, 1]
         );
     }
 
     #[test]
     fn test_sequency() {
-        assert_eq!(sequency(&mut [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]), [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]);
         assert_eq!(
-            sequency(&mut [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+            sequency(&mut [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+            [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+        );
+        assert_eq!(
+            sequency(&[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
             [1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0]
         );
         assert_eq!(
-            sequency(&mut [0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+            sequency(&[0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
             [1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0]
         );
         assert_eq!(
-            sequency(&mut [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0]),
+            sequency(&[0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0]),
             [1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0]
         );
         assert_eq!(
-            sequency(&mut [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]),
+            sequency(&[0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]),
             [1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0]
         );
         assert_eq!(
-            sequency(&mut [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0]),
+            sequency(&[0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0]),
             [1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0]
         );
         assert_eq!(
-            sequency(&mut [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]),
+            sequency(&[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0]),
             [1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0]
         );
         assert_eq!(
-            sequency(&mut [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]),
+            sequency(&[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]),
             [1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0]
         );
         assert_eq!(
-            sequency(&mut [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+            sequency(&[0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
             [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0]
         );
         assert_eq!(
-            sequency(&mut [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+            sequency(&[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
             [1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1]
         );
     }
 
     #[test]
     fn test_scaling() {
-        let mut input = vec![3, 6, 9];
+        let input = vec![3, 6, 9];
         let outcome = vec![1.0, 2.0, 3.0];
-        assert_eq!(scale(&mut input), outcome);
-        let mut input = vec![1.0, 2.0, 3.0, 4.0];
+        assert_eq!(scale(&input), outcome);
+        let input = vec![1.0, 2.0, 3.0, 4.0];
         let outcome = vec![0.25, 0.5, 0.75, 1.0];
-        assert_eq!(scale(&mut input), outcome);
-        let mut input = [3, 6, 9];
+        assert_eq!(scale(&input), outcome);
+        let input = [3, 6, 9];
         let outcome = vec![1.0, 2.0, 3.0];
-        assert_eq!(scale(&mut input), outcome);
+        assert_eq!(scale(&input), outcome);
     }
 
     #[test]
@@ -216,24 +218,25 @@ mod tests {
         assert!(!power_of_2(3));
         assert!(!power_of_2(5));
         assert!(!power_of_2(7));
-        assert!(!power_of_2(u32::MAX));
+        assert!(!power_of_2(usize::MAX));
     }
 
     #[test]
     #[should_panic]
     fn test_non_power_of_2_seq() {
-        sequency(&mut [0.0, 0.0, 1.0]);
+        sequency(&[0.0, 0.0, 1.0]);
     }
+ 
     // non-power-of-2 array
     #[test]
     #[should_panic]
     fn test_non_power_of_2_had() {
-        hadamard(&mut [0.0, 0.0, 1.0]);
+        hadamard(&[0.0, 0.0, 1.0]);
     }
 
     #[test]
     #[should_panic]
     fn test_empty_array() {
-        sequency::<i32>(&mut []);
+        sequency::<i32>(&[]);
     }
 }
